4a3cf9980a8b65388d868869c177e27fe33c02f2
Sanath Kumar R <112154986+sanathkumar-744@users.noreply.github.com>
2024-04-23 11:56:56 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -3,13 +3,14 @@ import datetime as dt
 import hashlib
 from flask import Flask, request, render_template, Response
 
-# Importing local functions
+# Importing local functions and classes
 from block import *
 from genesis import create_genesis_block
 from newBlock import next_block, add_block
 from getBlock import find_records
 from checkChain import check_integrity
-
+import smartpy
+from smartpy_functions import *
 # Flask declarations
 app = Flask(__name__)
 response = Response()
@@ -19,65 +20,81 @@ response.headers.add('Cache-Control', 'no-store, no-cache, must-revalidate, post
 blockchain = create_genesis_block()
 data = []
 
+# Function to display all blocks of the blockchain
+def display_blocks(blockchain):
+    blocks_info = []
+    for block in blockchain:
+        block_info = {
+            "Index": block.index,
+            "Timestamp": block.timestamp,
+            "Data": block.data,
+            "Previous Hash": block.previous_hash,
+            "Hash": block.hash
+        }
+        blocks_info.append(block_info)
+    return blocks_info
+
 # Default Landing page of the app
-@app.route('/',  methods = ['GET'])
+@app.route('/',  methods=['GET'])
 def index():
     return render_template("index.html")
 
 # Get Form input and decide what is to be done with it
-@app.route('/', methods = ['POST'])
+@app.route('/', methods=['POST'])
 def parse_request():
-    if(request.form.get("name")):
+    if request.form.get("name"):
         while len(data) > 0:
             data.pop()
         data.append(request.form.get("name"))
         data.append(str(dt.date.today()))
         return render_template("class.html",
-                                name = request.form.get("name"),
-                                date = dt.date.today())
+                                name=request.form.get("name"),
+                                date=dt.date.today())
 
-    elif(request.form.get("number")):
+    elif request.form.get("number"):
         while len(data) > 2:
             data.pop()
         data.append(request.form.get("course"))
         data.append(request.form.get("year"))
         return render_template("attendance.html",
-                                name = data[0],
-                                course = request.form.get("course"),
-                                year = request.form.get("year"),
-                                number = int(request.form.get("number")))
-    elif(request.form.get("roll_no1")):
+                                name=data[0],
+                                course=request.form.get("course"),
+                                year=request.form.get("year"),
+                                number=int(request.form.get("number")))
+    elif request.form.get("roll_no1"):
         while len(data) > 4:
             data.pop()
-        return render_template("result.html", result = add_block(request.form, data, blockchain))
+        return render_template("result.html", result=add_block(request.form, data, blockchain))
 
     else:
         return "Invalid POST request. This incident has been recorded."
 
 # Show page to get information for fetching records
-@app.route('/view.html',  methods = ['GET'])
+@app.route('/view.html',  methods=['GET'])
 def view():
     return render_template("class.html")
 
 # Process form input for fetching records from the blockchain
-@app.route('/view.html',  methods = ['POST'])
+@app.route('/view.html',  methods=['POST'])
 def show_records():
     data = []
     data = find_records(request.form, blockchain)
     if data == -1:
         return "Records not found"
     return render_template("view.html",
-                            name = request.form.get("name"),
-                            course = request.form.get("course"),
-                            year = request.form.get("year"),
-                            status = data,
-                            number = int(request.form.get("number")),
-                            date = request.form.get("date"))
+                            name=request.form.get("name"),
+                            course=request.form.get("course"),
+                            year=request.form.get("year"),
+                            status=data,
+                            number=int(request.form.get("number")),
+                            date=request.form.get("date"))
 
 # Show page with result of checking blockchain integrity
-@app.route('/result.html',  methods = ['GET'])
+@app.route('/result.html',  methods=['GET'])
 def check():
-    return render_template("result.html", result = check_integrity(blockchain))
+    return render_template("result.html", result=check_integrity(blockchain))
+
+
 
 # Start the flask app when program is executed
 if __name__ == "__main__":
